ed4a230b5b94c1cded7e4439da93279a7a15074e
Eric Pierce <wapcaplet88@gmail.com>
2012-07-04 16:08:45 -0600

    Add python logger

---
+++ logger.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# logger.py
+
+"""Log the serial output from the Arduino to a text file.
+"""
+
+import sys
+import serial
+
+def log_serial(filename, device='/dev/ttyACM0', baud=9600):
+    ser = serial.Serial(device, baud)
+    outfile = open(filename, 'w')
+    while True:
+        line = ser.readline()
+        outfile.write(line)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    log_serial(filename)
+
