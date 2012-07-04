1b1bbf959a1e9fcc940aeb0e660a90f9318ec53a
Eric Pierce <wapcaplet88@gmail.com>
2012-07-04 16:12:10 -0600

    Usage message, catch keyboard interrupt

--- logger.py
+++ logger.py
@@ -10,11 +10,17 @@ import serial
 def log_serial(filename, device='/dev/ttyACM0', baud=9600):
     ser = serial.Serial(device, baud)
     outfile = open(filename, 'w')
-    while True:
-        line = ser.readline()
-        outfile.write(line)
+    try:
+        while True:
+            line = ser.readline()
+            outfile.write(line)
+    except KeyboardInterrupt:
+        print("Quitting!")
+        outfile.close()
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: logger.py <filename>")
     filename = sys.argv[1]
     log_serial(filename)
 
