fb409f2a6a133cd1cded1640bdb1174670a4a251
Eric Pierce <wapcaplet88@gmail.com>
2012-07-07 19:19:49 -0600

    Just print output, don't write to a file

--- logger.py
+++ logger.py
@@ -1,29 +1,24 @@
 #! /usr/bin/env python
 # logger.py
 
-"""Log the serial output from the Arduino to a text file.
+"""Print the serial output from an Arduino.
 """
 
 import sys
 import serial
 from datetime import datetime
 
-def log_serial(filename, device='/dev/ttyACM0', baud=9600):
+def log_serial(device='/dev/ttyACM0', baud=9600):
+    print("Logging output from %s. Press Ctrl-C to stop.")
     ser = serial.Serial(device, baud)
-    outfile = open(filename, 'a')
     try:
         while True:
-            line = ser.readline()
+            line = ser.readline().strip()
             now = datetime.now()
             print("%s, %s" % (now, line))
-            outfile.write("%s, %s" % (now, line))
     except KeyboardInterrupt:
         print("Quitting!")
-        outfile.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: logger.py <filename>")
-    filename = sys.argv[1]
-    log_serial(filename)
+    log_serial()
 
