5996a2dfbf5b6a85abdefee03da9b65d09583467
Eric Pierce <wapcaplet88@gmail.com>
2012-07-08 22:21:15 -0600

    Polish up logger

--- logger.py
+++ logger.py
@@ -1,29 +1,50 @@
 #! /usr/bin/env python
 # logger.py
 
-"""Log the serial output from the Arduino to a text file.
+"""Print the serial output from an Arduino, with timestamps on each line.
+
+Usage:
+
+    arlog.py [device] [baud]
+
+Examples:
+
+    arlog.py                  # Log /dev/ttyACM0 at 9600 baud
+    arlog.py /dev/tty23       # Log /dev/tty23 at 9600 baud
+    arlog.py /dev/tty23 2400  # Log /dev/tty23 at 2400 baud
 """
 
 import sys
 import serial
 from datetime import datetime
 
-def log_serial(filename, device='/dev/ttyACM0', baud=9600):
-    ser = serial.Serial(device, baud)
-    outfile = open(filename, 'a')
+def log_serial(device, baud):
+    """Log output from a serial device at the given baud rate."""
     try:
-        while True:
-            line = ser.readline()
-            now = datetime.now()
-            print("%s, %s" % (now, line))
-            outfile.write("%s, %s" % (now, line))
-    except KeyboardInterrupt:
-        print("Quitting!")
-        outfile.close()
+        ser = serial.Serial(device, baud)
+    except serial.serialutil.SerialException, e:
+        print("Problem: %s" % e)
+        return
+
+    while True:
+        line = ser.readline()
+        now = datetime.now()
+        print("%s, %s" % (now, line))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: logger.py <filename>")
-    filename = sys.argv[1]
-    log_serial(filename)
+    device = '/dev/ttyACM0'
+    baud = 9600
+
+    if len(sys.argv) >= 2:
+        device = sys.argv[1]
+    if len(sys.argv) == 3:
+        baud = int(sys.argv[2])
+
+    print("Logging output from %s at %s baud" % (device, baud))
+    print("Press Ctrl-C to quit.")
+
+    try:
+        log_serial(device, baud)
+    except KeyboardInterrupt:
+        print("Quitting!")
 
